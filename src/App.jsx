c81a82f46f836e0f0e6ea7916e82edd1444daf6a
EsneiderCdD <ecd.97@hotmail.com>
2025-04-22 08:10:07 -0500

    Estudiando props

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,45 @@
 
-import PropCard from "./clases/react_notes/props/Mnemósine/PropCard";
+import DiosesOlimpo from "./clases/react_notes/props/Mnemósine/PropCard";
+import Zeus from "./assets/imagenes/Zeus.png";
+
 
 
 function App() {
 
+    const familia = [
+    { pareja: "Hera", 
+        hijos: [
+            { nombre: "Ares", rol: "XYZ"},
+            { nombre: "Hebe", rol: "XYZ"},
+            { nombre: "Hefesto", rol: "XYZ"},
+            { nombre: "Heris", rol: "XYZ"},
+        ]
+    },
+    { pareja: "Otras Diosas", 
+        hijos: [
+            { nombre: "Las nueve musas", rol: "XYZ" },
+            { nombre: "Atenea", rol: "XYZ"}, 
+            { nombre: "Apolo y Artemisa", rol: "XYZ"},
+        ]
+    },
+    { pareja: "Humanas", 
+        hijos: [
+            { nombre: "Hercules", rol: "XYZ"}, 
+            { nombre: "Helena de Troya", rol: "XYZ"}, 
+            { nombre: "Dionisio", rol: "XYZ"},
+        ]
+    },
+    ];
+
     return (
 
-    <PropCard 
-        nombre= "Mnemósine"
-        edad= {20}   
-        descripcion= "El Dios de la memoria ha regresado de nuevo"
-        imagen= "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSJfkmb9u3Gr-TSt1Gz6zRb4v-65xb1WMfJRg&s"
+    <DiosesOlimpo 
+        nombre= "Zeus"
+        edad= {99}   
+        descripcion= "Dios del olimpo"
+        imagen= {Zeus}
+        familia= {familia}
+        
     />  
 
     )
--- src/clases/react_notes/props/Mnemósine/PropCard.jsx
+++ src/clases/react_notes/props/Mnemósine/PropCard.jsx
@@ -1,40 +1,38 @@
 import React from "react";
 
-function PropCard ({nombre, edad, descripcion, imagen }) {
-    return (
-        <div>
-            <p>{nombre}</p>
-            <p>{edad}</p>
-            <p>{descripcion}</p>
-            <img src={imagen} alt="Imagen" />
-        </div>
-    );
-}
+function DiosesOlimpo ({imagen, nombre, edad, descripcion, familia, }) {
 
-export default PropCard
-
-{/* 
-
-import PropCard from "./clases/react_notes/props/Mnemósine/PropCard";
-
-
-function App() {
+    const estilos = {
+        fontSize : "30px"
+        
+    }
 
     return (
+        <div style = {{ width: "500px", height: "500px", backgroundColor:"gray", display: "flex", flexDirection: "column", alignItems: "center", padding: "50px", borderRadius: "35%"}}>
+            <img src={imagen} alt="Imagen" className="img-dios" style={{width: "250px", height: "250px",borderRadius: "50%" }} />
+            <p style={estilos}>{nombre} </p>
+            <p style={estilos}>{edad} </p>
+            <p style={estilos}>{descripcion} </p>
+            {familia.map((mujer, index) => (
+            <div key={index}>
+                <h3>{mujer.pareja}</h3>
+                <ul>
+                {mujer.hijos.map((hijo, i) => (
+                    <li key={i}>{hijo.nombre} - {hijo.rol}</li>
+                ))}
+                </ul>
+            </div>
+            ))}
+        
+                    </div>
+                );
+            }
 
-    <PropCard 
-        nombre= "Mnemósine"
-        edad= {20}   
-        descripcion= "El Dios de la memoria ha regresado de nuevo"
-        imagen= "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSJfkmb9u3Gr-TSt1Gz6zRb4v-65xb1WMfJRg&s"
-    />  
-
-    )
-}
-
-export default App
+            export default DiosesOlimpo
 
 
-    
-    
-    */}
+
+{/*
+ */}
+
+
