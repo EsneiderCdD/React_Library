69475962232e3752f805d3d35e4e1e686dd39ccf
EsneiderCdD <ecd.97@hotmail.com>
2025-05-11 18:40:08 -0500

    Framer Motion

---
+++ src/clases/framer_motion/Principiante/Intro.jsx
@@ -0,0 +1,12 @@
+// Framer Motion es una librería de animaciones para React creada por el equipo de Framer
+// npm install framer-motion --save
+
+
+/* Estructura basica:
+
+<motion.[element]
+  initial={{ ... }}
+  animate={{ ... }}
+  transition={{ ... }}
+/> 
+*/
