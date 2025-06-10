dc436856ac598aebb8c105695ce1fcdea8195a16
EsneiderCdD <ecd.97@hotmail.com>
2025-06-10 09:51:48 -0500

    estudiando

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,35 @@
 import { useState } from "react";
 
 function EstadosYEventos () {
+    const [cajas, setCajas] = useState([
+        {id: 1, mensaje: "", color: "red"},
+        {id: 2, mensaje: "", color: "green"},
+    ])
 
     const [efectosSeleccionados, setEfectosSeleccionados] = useState([]);
     const [cajasSeleccionadas, setCajasSeleccionadas] = useState([]);
     const [mensaje, setMensaje] = useState("");
 
-    const toggleCajasSleccionadas = (id) => {
-        if (cajasSeleccionadas.includes(id)) {
-            setCajasSeleccionadas(cajasSeleccionadas.filter((e) => e !== id));
+    const toggleSeleccionEfecto = (efectoId) => {
+        if (efectosSeleccionados.includes(efectoId)) {
+            setEfectosSeleccionados(efectosSeleccionados.filter((e) => e !== efectoId));
         } else {
-            setCajasSeleccionadas ([...cajasSeleccionadas, id])
-        };        
-    }
-    const Boton = ({texto, descripcion}) => {
+            setEfectosSeleccionados([...efectosSeleccionados, efectoId]);
+        };
+     }
+         const toggleSeleccionCaja = (id) => {
+        if (cajasSeleccionadas.includes(id)) {
+            setCajasSeleccionadas(cajasSeleccionadas.filter((e) => e !== efectoId));
+        } else {
+            setCajasSeleccionadas([...cajasSeleccionadas, id]);
+        };
+     }
+
+    const Boton = ({texto, descripcion, id}) => {
         return (
-            <div style={{
+            <div 
+                onClick = {() => toggleSeleccionEfecto(id)}
+                style={{
                 height: "50px",
                 width: "200px",
                
@@ -43,6 +57,12 @@ function EstadosYEventos () {
             <h1>Estados y Eventos</h1>
             <div style={{display: "flex" }}>
                 <Boton texto="Boton 1" descripcion="Descripcion del Boton 1"/>
+                <input 
+                    type="text"
+                    placeholder="Escribe algo"
+                    onChange={(e) => setMensaje(e.target.value)}
+                    value={mensaje}
+                    />
                 <Boton texto="Boton 2"/>
             </div> 
             <div style={{display: "flex" }}>
