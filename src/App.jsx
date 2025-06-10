efd507437296e118537e942c72436d598a2a858a
EsneiderCdD <ecd.97@hotmail.com>
2025-06-10 01:56:34 -0500

    toggle

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,56 @@
+import { useState } from "react";
+
 function EstadosYEventos () {
 
+    const [efectosSeleccionados, setEfectosSeleccionados] = useState([]);
+    const [cajasSeleccionadas, setCajasSeleccionadas] = useState([]);
+    const [mensaje, setMensaje] = useState("");
+
+    const toggleCajasSleccionadas = (id) => {
+        if (cajasSeleccionadas.includes(id)) {
+            setCajasSeleccionadas(cajasSeleccionadas.filter((e) => e !== id));
+        } else {
+            setCajasSeleccionadas ([...cajasSeleccionadas, id])
+        };        
+    }
     const Boton = ({texto, descripcion}) => {
         return (
             <div style={{
                 height: "50px",
                 width: "200px",
+               
             }}>
                 <div><button>{texto}</button></div>
                 <span>{descripcion}</span>
             </div>
+            
+
         );
     }
-     const Caja = ({}) => {
+    const Caja = ({}) => {
         return (
-            <div
-            style={{
+            <div style={{
                 background: "red",
                 height: "200px",
-                width: "200px"
+                width: "200px",
+                
             }}>
-               
             </div>
         );
     }
     return (
-      <div 
-      style={{display: "flex", flexDirection: "column", alignItems: "center"}}>
-        <h1>Estados y Eventos</h1>
-        <div style={{display: "flex"}}>
-            <Boton texto="Boton 1" descripcion="Descripcion 1" />
-            <Boton texto="Boton 2" descripcion="Descripcion 2" />
+        <div style={{display: "flex", flexDirection: "column", alignItems: "center"}}>
+            <h1>Estados y Eventos</h1>
+            <div style={{display: "flex" }}>
+                <Boton texto="Boton 1" descripcion="Descripcion del Boton 1"/>
+                <Boton texto="Boton 2"/>
+            </div> 
+            <div style={{display: "flex" }}>
+                <Caja/>
+                <Caja/>
+            </div> 
         </div>
-        <div style={{display: "flex"}}>
-            <Caja />
-            <Caja />
-        </div>
-      </div>
+
     );
 }
 export default EstadosYEventos
